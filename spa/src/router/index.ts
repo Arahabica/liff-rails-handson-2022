9918eaceb049e989b0960a1ea82cfbc39634c2c7
Arahabica <r.sasage@gmail.com>
2022-01-06 16:40:04 +0900

    ちょっとした修正

--- spa/src/router/index.ts
+++ spa/src/router/index.ts
@@ -6,16 +6,9 @@ const routes: Array<RouteRecordRaw> = [
   {
     path: '/',
     name: 'Home',
-    component: Home
+    component: Home,
+    meta: { title: '鎌倉七福神巡り' }
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
 ]
 
 const router = createRouter({
